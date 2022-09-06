73640c1b40e41e4d1133a491daede9713aab9f63
aallbrig <aallbrig@gmail.com>
2022-09-06 23:24:50 +0000

    Deploying to github-pages from @ aallbrig/helms-deep-tower-defense@8d5803ac264f04ee4a93c9583aae227546027495 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -1,13 +1,13 @@
 var canvas = document.querySelector("#unity-canvas");
 
 var config = {
-    dataUrl: "Build/WebGL.data.unityweb",
-    frameworkUrl: "Build/WebGL.framework.js.unityweb",
-    codeUrl: "Build/WebGL.wasm.unityweb",
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "helms-deep-tower-defense",
-    productVersion: "0.0.12",
+    productVersion: "0.0.13",
     devicePixelRatio: 1,
 }
 
