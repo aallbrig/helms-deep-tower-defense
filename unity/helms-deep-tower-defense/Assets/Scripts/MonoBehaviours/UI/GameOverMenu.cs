cb40288564de8c0374526f08c0dd1cd3bfc22590
aallbright <aallbrig@gmail.com>
2022-09-30 14:55:40 -0400

    Game over menu activates when game referee declares all castles destroyed and game over

---
+++ unity/helms-deep-tower-defense/Assets/Scripts/MonoBehaviours/UI/GameOverMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MonoBehaviours.Systems;
+using UnityEngine;
+
+namespace MonoBehaviours.UI
+{
+    public class GameOverMenu : MonoBehaviour
+    {
+        public event Action GameOverMenuIsActivated;
+        public GameObject gameOverMenuCanvas;
+        private bool _allCastlesDestroyed = false;
+        private bool _gameIsOver = false;
+        private void Start()
+        {
+            gameOverMenuCanvas.SetActive(false);
+            var referee = FindObjectOfType<GameReferee>();
+            referee.AllCastlesDestroyed += () =>
+            {
+                _allCastlesDestroyed = true;
+                if (ShouldActivateMenu()) ActivateMenu();
+            };
+            referee.GameIsOver += () =>
+            {
+                _gameIsOver = true;
+                if (ShouldActivateMenu()) ActivateMenu();
+            };
+        }
+        private void ActivateMenu()
+        {
+            if (gameOverMenuCanvas == null) return;
+
+            gameOverMenuCanvas.SetActive(true);
+            GameOverMenuIsActivated?.Invoke();
+        }
+        private bool ShouldActivateMenu() => _allCastlesDestroyed && _gameIsOver;
+    }
+}
---
+++ unity/helms-deep-tower-defense/Assets/Tests/PlayMode/Scenarios/ForGameMenuScreen/GameOverMenuFacts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using Model.Factories;
+using MonoBehaviours.UI;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode.Scenarios.ForGameMenuScreen
+{
+    public class GameOverMenuFacts: ScenarioTest
+    {
+        private readonly PrefabSpawner _gameOverMenuSpawner = new PrefabSpawner("Prefabs/UI/Game Over Menu");
+        private PrefabSpawner _gameRefereeSpawner = new PrefabSpawner("Prefabs/Systems/Game Referee");
+        private PrefabSpawner _castleSpawner = new PrefabSpawner("Prefabs/Castle");
+        [UnityTest]
+        public IEnumerator GameOverMenu_Activates_WhenGameRefereeDeclaresGameOver()
+        {
+            var gameOverMenuGameObject = _gameOverMenuSpawner.Spawn();
+            var gameOverMenu = gameOverMenuGameObject.GetComponent<GameOverMenu>();
+            var gameOverMenuActivated = false;
+            gameOverMenu.GameOverMenuIsActivated += () => gameOverMenuActivated = true;
+            CleanupAtEnd(gameOverMenuGameObject);
+            CleanupAtEnd(_gameRefereeSpawner.Spawn());
+            var castleGameObject = _castleSpawner.Spawn();
+            CleanupAtEnd(castleGameObject);
+            TestCameraLookAt(castleGameObject.transform);
+            yield return null;
+            castleGameObject.GetComponent<MonoBehaviours.Castle>().Kill();
+
+            Assert.IsTrue(gameOverMenuActivated);
+        }
+    }
+}
