c9fc36cd746be42cf789e967593a5f19614df4e4
Andrew Allbright <aallbrig@gmail.com>
2022-10-04 22:23:30 -0400

    Victory menu test fix

--- unity/helms-deep-tower-defense/Assets/Tests/PlayMode/Scenarios/ForVictoryMenu/VictoryMenuFacts.cs
+++ unity/helms-deep-tower-defense/Assets/Tests/PlayMode/Scenarios/ForVictoryMenu/VictoryMenuFacts.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Linq;
 using Model.Combat;
 using Model.Factories;
@@ -7,19 +6,18 @@ using MonoBehaviours.UI;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using Object = UnityEngine.Object;
 
 namespace Tests.PlayMode.Scenarios.ForVictoryMenu
 {
-    public class VictoryMenuFacts: ScenarioTest
+    public class VictoryMenuFacts : ScenarioTest
     {
-        private readonly PrefabSpawner _victoryMenuSpawner = new PrefabSpawner("Prefabs/UI/Victory Menu");
-        private readonly PrefabSpawner _refereeSpawner = new PrefabSpawner("Prefabs/Systems/Game Referee");
-        private readonly PrefabSpawner _pathSpawner = new PrefabSpawner("Prefabs/Paths/Test Path");
         private readonly PrefabSpawner _pathFollowerSpawnerSpawner = new PrefabSpawner("Prefabs/Spawners/Test Wave");
+        private readonly PrefabSpawner _pathSpawner = new PrefabSpawner("Prefabs/Paths/Test Path");
+        private readonly PrefabSpawner _refereeSpawner = new PrefabSpawner("Prefabs/Systems/Game Referee");
+        private readonly PrefabSpawner _victoryMenuSpawner = new PrefabSpawner("Prefabs/UI/Victory Menu");
 
         [UnityTest]
-        public IEnumerator VictoryMenuFactsWithEnumeratorPasses()
+        public IEnumerator VictoryMenu_Activates_OnWinCondition()
         {
             var victoryMenu = _victoryMenuSpawner.Spawn();
             var victoryMenuComponent = victoryMenu.GetComponent<VictoryMenu>();
@@ -29,6 +27,9 @@ namespace Tests.PlayMode.Scenarios.ForVictoryMenu
             _pathSpawner.Spawn();
             _pathFollowerSpawnerSpawner.Spawn();
             yield return null;
+            Time.timeScale = 10.0f;
+            yield return new WaitForSeconds(1.1f);
+            Time.timeScale = 1.0f;
 
             foreach (var killable in Object.FindObjectsOfType<MonoBehaviour>().OfType<IKillable>())
                 killable.Kill();
