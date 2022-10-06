655e06288af1773368c3341957a736a4afee1c8e
Andrew Allbright <aallbrig@gmail.com>
2022-10-06 09:31:10 -0400

    Faff about with the playables API

--- unity/helms-deep-tower-defense/Assets/Tests/PlayMode/Scenarios/ForWaveDirector/WaveDirectorFacts.cs
+++ unity/helms-deep-tower-defense/Assets/Tests/PlayMode/Scenarios/ForWaveDirector/WaveDirectorFacts.cs
@@ -17,6 +17,27 @@ namespace Tests.PlayMode.Scenarios.ForWaveDirector
             yield return null;
             Assert.IsNotNull(waveDirector);
             Assert.IsNotNull(directorComponent);
+            Assert.IsTrue(directorComponent.playOnAwake);
+
+            /*
+            // TODO: I should be able to assert that bound game objects start off deactivated
+            var gameObject1 = new GameObject();
+            var gameObject2 = new GameObject();
+            var gameObject3 = new GameObject();
+            Selection.activeGameObject = waveDirector;
+            using var playableAssetOutputs = directorComponent.playableAsset.outputs.GetEnumerator();
+            directorComponent.SetGenericBinding(playableAssetOutputs.Current.sourceObject, gameObject1);
+            playableAssetOutputs.MoveNext();
+            directorComponent.SetGenericBinding(playableAssetOutputs.Current.sourceObject, gameObject2);
+            playableAssetOutputs.MoveNext();
+            directorComponent.SetGenericBinding(playableAssetOutputs.Current.sourceObject, gameObject3);
+            directorComponent.RebindPlayableGraphOutputs();
+
+            directorComponent.time = directorComponent.initialTime;
+            Assert.IsFalse(gameObject3.activeSelf);
+            Assert.IsFalse(gameObject2.activeSelf);
+            Assert.IsFalse(gameObject1.activeSelf);
+            */
         }
     }
 }
